8f29bf6073ff2867877fef4cadf19aac2441f67e
Samuelle MOLLARD <s18molla@pc-elec-188.priv.enst-bretagne.fr>
2021-03-19 10:58:53 +0100

    Various files, some taken from the internet to try and compile a compute shader. Since this is not possible on the hardware, I am giving up with opengl

--- temp/test.cpp
+++ temp/test.cpp
@@ -5,36 +5,81 @@
 #include <vector>
 #include <chrono>
 #include <GL/glew.h>
+#include <string>
+#include <fstream>
+#include <GL/glut.h>
 
 bool random_bool() {
 	return rand() > (RAND_MAX / 2);
 }
 
-void printWorkGroupsCapabilities() {
-  int workgroup_count[3];
-  int workgroup_size[3];
-  int workgroup_invocations;
+std::string readFile(const char *filePath) {
+    std::string content;
+    std::ifstream fileStream(filePath, std::ios::in);
 
-  glGetIntegeri_v(GL_MAX_COMPUTE_WORK_GROUP_COUNT, 0, &workgroup_count[0]);
-  glGetIntegeri_v(GL_MAX_COMPUTE_WORK_GROUP_COUNT, 1, &workgroup_count[1]);
-  glGetIntegeri_v(GL_MAX_COMPUTE_WORK_GROUP_COUNT, 2, &workgroup_count[2]);
+    if(!fileStream.is_open()) {
+        std::cerr << "Could not read file " << filePath << ". File does not exist." << std::endl;
+        return "";
+    }
 
-  printf ("Taille maximale des workgroups:\n\tx:%u\n\ty:%u\n\tz:%u\n",
-  workgroup_size[0], workgroup_size[1], workgroup_size[2]);
+    std::string line = "";
+    while(!fileStream.eof()) {
+        std::getline(fileStream, line);
+        content.append(line + "\n");
+    }
 
-  glGetIntegeri_v(GL_MAX_COMPUTE_WORK_GROUP_SIZE, 0, &workgroup_size[0]);
-  glGetIntegeri_v(GL_MAX_COMPUTE_WORK_GROUP_SIZE, 1, &workgroup_size[1]);
-  glGetIntegeri_v(GL_MAX_COMPUTE_WORK_GROUP_SIZE, 2, &workgroup_size[2]);
-
-  printf ("Nombre maximal d'invocation locale:\n\tx:%u\n\ty:%u\n\tz:%u\n",
-  workgroup_size[0], workgroup_size[1], workgroup_size[2]);
-
-  glGetIntegerv (GL_MAX_COMPUTE_WORK_GROUP_INVOCATIONS, &workgroup_invocations);
-  printf ("Nombre maximum d'invocation de workgroups:\n\t%u\n", workgroup_invocations);
+    return content;
 }
 
+void gpu_compute() {
 
-int main() {
+	auto gComputeProgram = glCreateProgram();
+	GLuint mComputeShader = glCreateShader(GL_COMPUTE_SHADER);
+
+	const char *shader_file = readFile("compute.glsl").c_str();
+
+	glShaderSource(mComputeShader, 1, &shader_file, NULL);
+	glCompileShader(mComputeShader);
+
+	int rvalue;
+	int length;
+	glGetShaderiv(mComputeShader, GL_COMPILE_STATUS, &rvalue);
+	if (!rvalue) {
+		GLint maxLength = 0;
+		glGetShaderiv(mComputeShader, GL_INFO_LOG_LENGTH, &maxLength);
+
+		// The maxLength includes the NULL character
+		std::vector<GLchar> errorLog(maxLength);
+		glGetShaderInfoLog(mComputeShader, maxLength, &maxLength, &errorLog[0]);
+		for (int i = 0; i < errorLog.size(); ++i) {
+			std::cerr << errorLog[i];
+		}
+		std::cerr << std::endl;
+		return ;
+	}
+
+	// Attach and link the shader against to the compute program
+	glAttachShader(gComputeProgram, mComputeShader);
+	glLinkProgram(gComputeProgram);
+
+	// Check if there were some issues when linking the shader.
+	glGetProgramiv(gComputeProgram, GL_LINK_STATUS, &rvalue);
+	if (!rvalue) {
+	       printf("Error get program iv : %d", rvalue);
+	       return ;
+	}
+}
+
+int main(int argc, char* argv[]) {
+	glutInit(&argc, argv);
+	glutCreateWindow("Just to load glew");
+	GLenum err = glewInit();
+	if (GLEW_OK != err) {
+		std::cerr << "Error initialising glew: " << glewGetErrorString(err) << std::endl;
+	}
+	else {
+		std::cerr << "Loaded glew" << std::endl;
+	}
 	std::vector<bool> tab(10000000, false);
 	for (int i = 0; i < tab.size(); ++i) {
 		tab[i] = random_bool();
@@ -50,9 +95,15 @@ int main() {
 	}
 
 	auto t2 = std::chrono::high_resolution_clock::now();
+
+	gpu_compute();
+
+	auto t3 = std::chrono::high_resolution_clock::now();
 	std::chrono::duration<float> duration_s = t2 - t1;
 	std::cout << "Duration for CPU: " << duration_s.count() << std::endl;
 	std::cout << "CPU result : " << sum << std::endl;
 
-	printWorkGroupsCapabilities();
+	//duration_s = t3 - t2;
+	//std::cout << "Duration for GPU: " << duration_s.count() << std::endl;
+
 }
